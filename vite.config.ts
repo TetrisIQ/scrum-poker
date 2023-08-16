ef8c49231918767bc0b46e37d7095bd5a1684b57
TetrisIQ <dev@tetrisiq.de>
2023-08-16 11:01:44 +0200

    build: add auth token for sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "poddle",
-            project: "poker",
-            url: "https://glitchtip.poddle.network/"
-        }
-    }), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'poddle',
+				project: 'poker',
+				url: 'https://glitchtip.poddle.network/',
+				authToken: 'process.env.SENTRY_AUTH_TOKEN'
+			}
+		}),
+		sveltekit()
+	],
 	server: {
 		host: '127.0.0.1',
 		port: 8765
