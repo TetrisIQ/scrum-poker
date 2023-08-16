028ee89987103738e197b66028f8a69bd3bb9263
TetrisIQ <dev@tetrisiq.de>
2023-08-16 12:06:43 +0200

    feat: skip sentry sourcemap upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
+import type { SentrySvelteKitPluginOptions } from '@sentry/sveltekit/types/vite/sentryVitePlugins';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import type { Plugin } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'poddle',
-				project: 'poker',
-				url: 'https://glitchtip.poddle.network/',
-				authToken: 'process.env.SENTRY_AUTH_TOKEN'
-			}
-		}),
+		import.meta.url.includes('private')
+			? sentrySvelteKit({
+					sourceMapsUploadOptions: {
+						org: 'poddle',
+						project: 'poker',
+						url: 'https://glitchtip.poddle.network/'
+					}
+			  })
+			: pass(),
 		sveltekit()
 	],
 	server: {
@@ -19,3 +22,13 @@ export default defineConfig({
 		port: 8765
 	}
 });
+
+function pass(options?: SentrySvelteKitPluginOptions): Promise<Plugin[]> {
+	return new Promise((resolveOuter) => {
+		resolveOuter(
+			new Promise((resolveInner) => {
+				setTimeout(resolveInner, 1000);
+			})
+		);
+	});
+}
